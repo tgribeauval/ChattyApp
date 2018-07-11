abd41e8bdc1f8415a9ca47d55de8aab074fce6c5
Thierry Gribeauval <t.gribeauval11@gmail.com>
2018-07-11 20:40:41 +0000

    Displaying incoming messages

--- chatty_server/chatty_server.js/server.js
+++ chatty_server/chatty_server.js/server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const SocketServer = require('ws').Server;
-
+const uuidv4 = require('uuid/v4');
 // Set the port to 3001
 const PORT = 3001;
 
@@ -15,14 +15,22 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+      client.send(data);
+  });
+};
+
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
-ws.on('message', function(event) {
-  var msg = JSON.parse(event)
-  console.log(msg);
+ws.on('message', function(message) {
+  let msg = JSON.parse(message)
+  msg["id"]= uuidv4()
+  wss.broadcast(JSON.stringify(msg))
+  console.log('msg', msg);
 })
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
